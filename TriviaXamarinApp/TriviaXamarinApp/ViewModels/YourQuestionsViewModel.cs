23f06e258f8ea1510028d2a4233e8382974f6d6c
LiamRaz <liamraz123@gmail.com>
2021-05-07 16:30:54 +0300

    version 5 - beginning of your questions page

---
+++ TriviaXamarinApp/TriviaXamarinApp/ViewModels/YourQuestionsViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using TriviaXamarinApp.Services;
+using TriviaXamarinApp.Models;
+using System.Threading.Tasks;
+using TriviaXamarinApp.Views;
+
+namespace TriviaXamarinApp.ViewModels
+{
+    class YourQuestionsViewModel:BaseViewModel
+    {
+
+        public YourQuestionsViewModel()
+        {
+            DeleteQuestion = new Command(Delete);
+            GoToEditCommand = new Command(GoToEdit);
+        }
+
+        private void GoToEdit(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private async void Delete()
+        {
+            TriviaWebAPIProxy proxy = TriviaWebAPIProxy.CreateProxy();
+            bool b = await proxy.DeleteQuestion(ChosenQuestion);
+
+        }
+
+        #region Properties
+
+        private AmericanQuestion chosenQuestion;
+
+        public AmericanQuestion ChosenQuestion
+        {
+            get => chosenQuestion;
+            set
+            {
+                if(value != chosenQuestion)
+                {
+                    chosenQuestion = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand DeleteQuestion { get; set; }
+
+        public ICommand GoToEditCommand { get; set; }
+
+        #endregion
+
+        #region Events
+
+        public event Action<Page> Push;
+
+        #endregion
+
+    }
+}
