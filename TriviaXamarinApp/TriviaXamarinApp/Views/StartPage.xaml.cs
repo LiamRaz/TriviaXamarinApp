8414cd99641e2cc7c796fb7fea35f53512415c1e
LiamRaz <liamraz123@gmail.com>
2021-05-09 16:22:42 +0300

    version 10

--- TriviaXamarinApp/TriviaXamarinApp/Views/StartPage.xaml.cs
+++ TriviaXamarinApp/TriviaXamarinApp/Views/StartPage.xaml.cs
@@ -19,16 +19,31 @@ namespace TriviaXamarinApp.Views
             InitializeComponent();
             BindingContext = new StartPageViewModel();
 
+
+
             if (((App)App.Current).CurrentUser != null)
             {
-                ButtonsGrid.Children.Add(new Button { BackgroundColor = Color.Gold, Text = "Play", CornerRadius = 20, HorizontalOptions = LayoutOptions.Center, Margin = 30, WidthRequest = 150, FontAttributes = FontAttributes.Bold, FontSize = 30 },0,0);
-                ButtonsGrid.Children.Add(new Button { BackgroundColor = Color.Gold, Text = "Logout", CornerRadius = 20, HorizontalOptions = LayoutOptions.Center, Margin = 30, WidthRequest = 150, FontAttributes = FontAttributes.Bold, FontSize = 30 },0,1);
-                ButtonsGrid.Children.Add(new Button { BackgroundColor = Color.Gold, Text = "Edit", CornerRadius = 20, HorizontalOptions = LayoutOptions.Center, Margin = 30, WidthRequest = 150, FontAttributes = FontAttributes.Bold, FontSize = 30 },0,2);
+                Button b = new Button { BackgroundColor = Color.Gold, Text = "Play", CornerRadius = 20, HorizontalOptions = LayoutOptions.Center, Margin = 10, WidthRequest = 100, FontAttributes = FontAttributes.Bold, FontSize = 15 };
+                b.SetBinding(Button.CommandProperty, "PlayCommand");
+                ButtonsGrid.Children.Add(b,0,0);
+                
+                Button b2 = new Button { BackgroundColor = Color.Gold, Text = "Logout", CornerRadius = 20, HorizontalOptions = LayoutOptions.Center, Margin = 10, WidthRequest = 100, FontAttributes = FontAttributes.Bold, FontSize = 15 };
+                b2.SetBinding(Button.CommandProperty, "LogoutCommand");
+                ButtonsGrid.Children.Add(b2, 0, 1) ;
+
+                Button b3 = new Button { BackgroundColor = Color.Gold, Text = "Edit", CornerRadius = 20, HorizontalOptions = LayoutOptions.Center, Margin = 10, WidthRequest = 100, FontAttributes = FontAttributes.Bold, FontSize = 15 };
+                b3.SetBinding(Button.CommandProperty, "EditCommand");
+                ButtonsGrid.Children.Add(b3,0,2);
             }
             else
             {
-                ButtonsGrid.Children.Add(new Button { BackgroundColor = Color.Gold, Text = "Play", CornerRadius = 20, HorizontalOptions = LayoutOptions.Center, Margin = 30, WidthRequest = 150, FontAttributes = FontAttributes.Bold, FontSize = 30 },0,0);
-                ButtonsGrid.Children.Add(new Button { BackgroundColor = Color.Gold, Text = "Login", CornerRadius = 20, HorizontalOptions = LayoutOptions.Center, Margin = 30, WidthRequest = 150, FontAttributes = FontAttributes.Bold, FontSize = 30 },0,1);
+                Button b = new Button { BackgroundColor = Color.Gold, Text = "Play", CornerRadius = 20, HorizontalOptions = LayoutOptions.Center, WidthRequest = 100, FontAttributes = FontAttributes.Bold, FontSize = 15 };
+                b.SetBinding(Button.CommandProperty, "PlayCommand");
+                ButtonsGrid.Children.Add(b,0,0);
+
+                Button b1 = new Button { BackgroundColor = Color.Gold, Text = "Login", CornerRadius = 20, HorizontalOptions = LayoutOptions.Center, WidthRequest = 100, FontAttributes = FontAttributes.Bold, FontSize = 15 };
+                b1.SetBinding(Button.CommandProperty, "LoginCommand");
+                ButtonsGrid.Children.Add(b1,0,1);
             }
 
         }
